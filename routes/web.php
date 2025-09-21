35ca5db415af8abcaab8851017977c7f07d5b9f4
HesselFarras <hesselfarras1@gmail.com>
2025-09-21 13:34:37 +0700

    edit controller

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
